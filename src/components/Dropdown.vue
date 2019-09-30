d07c42940b2f972314d9870e1fc35d15f45d47cf
unknown <pan_rostislav@ukr.net>
2019-09-30 23:55:43 +0300

    add dropdown

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="mdc-select mdc-select--outlined">
+  <i class="mdc-select__dropdown-icon"></i>
+  <select class="mdc-select__native-control">
+    <option :key="index" v-for="(option, index) in data" value="faces">
+      {{option}}
+    </option>
+  </select>
+  <label class="mdc-floating-label mdc-floating-label--float-above">Тип контакта</label>
+  <div class="mdc-line-ripple"></div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      data: [
+        'Только физ.лица',
+        'Все',
+        'Компании',
+        'Гос.организации'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@material/select/mdc-select.scss";
+.mdc-select {
+  background-color: white !important;
+  border: 1px solid #E5E5E5;
+  border-radius: 6px;
+  width: 280px;
+  height: 40px;
+}
+.mdc-select__native-control {
+  padding-top: 0px !important;
+}
+
+option {
+  background-color:#F5F6FA;
+}
+.mdc-select__dropdown-icon {
+  bottom: 7px;
+}
+</style>
